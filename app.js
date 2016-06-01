571ad0373a2656c5f772776aa01163f7f4446913
Carl Tessier <doktorfrankenstein@gmail.com>
2016-06-01 11:57:39 -0400

    Initial commit: root

---
+++ app.js
@@ -0,0 +1,33 @@
+﻿(function (angular) {
+
+  angular.module("ng-gtfs", ["ngRoute", "ui.bootstrap"])
+    .config(routeConfig)
+    .value("_", window._)
+    .value("Papa", window.Papa)
+  ;
+
+  routeConfig.$inject = ["$routeProvider"];
+  function routeConfig($routeProvider)
+  {
+    $routeProvider
+      .when("/agency/:feedname/:agencyname", {
+        templateUrl: "partials/agency-info.html",
+        controller: "AgencyInfoCtrl",
+        controllerAs: "ctrl"
+      })
+      .when("/route/:feedname/:routeId", {
+        templateUrl: "partials/route-info.html",
+        controller: "RouteInfoCtrl",
+        controllerAs: "ctrl"
+      })
+      .otherwise({
+        templateUrl: "partials/agency-list.html",
+        controller: "AgencyListCtrl",
+        controllerAs: "ctrl"
+        //templateUrl: "partials/feed-list.html",
+        //controller: "FeedListCtrl",
+        //controllerAs: "feedList"
+      });
+  }
+
+})(window.angular);
