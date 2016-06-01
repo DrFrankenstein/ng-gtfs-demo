e85046cc325c165b7d7a9cdd8d89c75fa9656ffc
Carl Tessier <doktorfrankenstein@gmail.com>
2016-06-01 12:13:06 -0400

    Add references script for VS IntelliSense

---
+++ Scripts/_references.js
@@ -0,0 +1,29 @@
+///<reference path="~/vendor/lodash.js"/>
+///<reference path="~/vendor/jquery.js"/>
+///<reference path="~/vendor/js/bootstrap.js"/>
+///<reference path="~/vendor/papaparse.js"/>
+
+///<reference path="~/vendor/angular.js"/>
+///<reference path="~/vendor/angular-route.js"/>
+///<reference path="~/vendor/ui-bootstrap-tpls.js"/>
+
+///<reference path="~/app.js"/>
+///<reference path="~/services/fetchCsv.js"/>
+///<reference path="~/services/feeds.js"/>
+///<reference path="~/services/agency.js"/>
+///<reference path="~/services/routes.js"/>
+///<reference path="~/services/calendar.js"/>
+///<reference path="~/services/trips.js"/>
+///<reference path="~/services/stopTimes.js"/>
+
+/// <reference path="../filters/calendar.js" />
+///<reference path="~/filters/isoifyDate.js"/>
+
+///<reference path="~/directives/routeTable.js"/>
+///<reference path="~/directives/routeLabel.js"/>
+/// <reference path="../directives/tripTable.js" />
+
+///<reference path="~/controllers/feedListCtrl.js"/>
+///<reference path="~/controllers/agencyListCtrl.js"/>
+///<reference path="~/controllers/agencyInfoCtrl.js"/>
+///<reference path="~/controllers/routeInfoCtrl.js"/>
