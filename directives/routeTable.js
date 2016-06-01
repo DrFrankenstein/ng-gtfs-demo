1e429d12c12e0eecb2729cfab60ae3c8d84612b0
Carl Tessier <doktorfrankenstein@gmail.com>
2016-06-01 11:59:57 -0400

    Add directives

---
+++ directives/routeTable.js
@@ -0,0 +1,39 @@
+﻿(function (angular) {
+  "use strict";
+
+  angular.module("ng-gtfs")
+    .directive("routeTable", routeTable)
+  ;
+
+  routeTable.$inject = ["$location", "_", "routes"];
+  function routeTable($location, _, routes)
+  {
+    return {
+      templateUrl: "/partials/route-table.html",
+      scope: {
+        routes: "=",
+        feedname: "@",
+        agency: "@"
+      },
+      controller: RouteTableCtrl,
+      controllerAs: "ctrl",
+      bindToController: true
+    };
+
+    function RouteTableCtrl()
+    {
+      this.gotoRoute = function (route) {
+        $location.path("/route/" + this.feedname + "/" + route.route_id);
+      };
+
+      if (angular.isUndefined(this.routes))
+      {
+        routes.routesForAgency(this.feedname, this.agency).then(_.bind(
+          function (routeData) {
+            this.routes = routeData;
+          },
+          this));
+      }
+    }
+  }
+})(window.angular);
---
+++ directives/tripTable.js
@@ -0,0 +1,37 @@
+﻿(function (angular) {
+  "use strict";
+
+  angular.module("ng-gtfs")
+    .directive("tripTable", tripTable)
+  ;
+
+  tripTable.$inject = ["_", "trips"];
+  function tripTable(_, trips)
+  {
+    return {
+      templateUrl: "/partials/trip-table.html",
+      scope: {
+        trips: "=",
+        feedname: "@",
+        route: "@"
+      },
+      controller: TripTableCtrl,
+      controllerAs: "ctrl",
+      bindToController: true,
+      replace: true // play well with bootstrap
+    };
+
+    function TripTableCtrl()
+    {
+      if (angular.isUndefined(this.trips))
+      {
+        trips.tripsForRoute(this.feedname, this.route).then(_.bind(
+          function (tripData) {
+            this.trips = tripData;
+          },
+          this));
+      }
+    }
+  }
+  
+})(window.angular);
