4358c55b75773512fc2fa0b4b587301405dae137
Carl Tessier <doktorfrankenstein@gmail.com>
2016-06-01 12:00:52 -0400

    Add filters

---
+++ filters/isoifyDate.js
@@ -0,0 +1,15 @@
+﻿(function (angular) {
+  "use strict";
+
+  angular.module("ng-gtfs")
+    .filter("isoifyDate", isoifyDateFactory)
+  ;
+
+  function isoifyDateFactory()
+  { // Takes in a date in GTFS' format and converts it to ISO
+    return function isoifyDate(gtfsdate) {
+      return gtfsdate.replace(/(\d{4})(\d{2})(\d{2})/g, "$1-$2-$3");
+    };
+  }
+
+})(window.angular);
